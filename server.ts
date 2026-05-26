64fbf77236a53d736a6ccf9eecba830bf0396fbb
P-zetterberg <pontus.z94@gmail.com>
2026-05-26 13:48:06 +0200

    added missing separator

--- server.ts
+++ server.ts
@@ -5,10 +5,26 @@ import { uniqueNamesGenerator, adjectives, animals } from 'unique-names-generato
 import { handler } from './build/handler.js';
 
 const COLORS = [
-	'#f87171', '#fb923c', '#fbbf24', '#a3e635', '#34d399',
-	'#38bdf8', '#818cf8', '#a78bfa', '#f472b6', '#fb7185',
-	'#2dd4bf', '#facc15', '#60a5fa', '#c084fc', '#f97316',
-	'#4ade80', '#e879f9', '#67e8f9', '#fde68a', '#a5f3fc',
+	'#f87171',
+	'#fb923c',
+	'#fbbf24',
+	'#a3e635',
+	'#34d399',
+	'#38bdf8',
+	'#818cf8',
+	'#a78bfa',
+	'#f472b6',
+	'#fb7185',
+	'#2dd4bf',
+	'#facc15',
+	'#60a5fa',
+	'#c084fc',
+	'#f97316',
+	'#4ade80',
+	'#e879f9',
+	'#67e8f9',
+	'#fde68a',
+	'#a5f3fc'
 ];
 
 type ClientInfo = { color: string; name: string };
@@ -23,7 +39,7 @@ io.on('connection', (socket) => {
 	const name = uniqueNamesGenerator({
 		dictionaries: [adjectives, animals],
 		length: 2,
-		separator: ''
+		separator: ' '
 	});
 	clients.set(socket.id, { color, name });
 
